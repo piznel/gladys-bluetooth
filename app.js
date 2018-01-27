41b3b16fa04f09d008a5d0f0a28926536fdd2c76
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2018-01-27 17:51:41 +0100

    Fix bug, starting scan immediately

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const scan = require('./lib/scan.js');
 const deviceSeen = require('./lib/deviceSeen.js');
 const getDevices = require('./lib/getDevices.js');
 
+scan(deviceSeen);
+
 getDevices()
-    .then(() => {
-        scan(deviceSeen)
-    })
     .catch((err) => {
         console.log('Error while getting devices from Gladys');
         console.log(err);
     });
+
