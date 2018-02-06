2408279c126b527c6b702837f73633f45b8a47ab
Dubois Nicolas <dubois.nicolas@free.fr>
2018-02-06 22:01:45 +0100

    Add PG modifications (v0.2.0)

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+const config = require('./config.js');
 const scan = require('./lib/scan.js');
 const deviceSeen = require('./lib/deviceSeen.js');
+const getDevices = require('./lib/getDevices.js');
+const shared = require('./lib/shared.js');
 
 scan(deviceSeen);
+
+var services = config.services.split(',');
+
+services.forEach(function(service) {
+  getDevices(service)
+    .then((devices) => {
+      // saving devices by identifier
+      devices.forEach((device) => {
+        shared.devices[device.identifier] = device;
+      });
+    })
+    .catch((err) => {
+      console.log(`Error while getting ${service} devices from Gladys`);
+      console.log(err);
+    });
+});
--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    scanInterval: process.env.BLUETOOTH_SCAN_INTERVAL || 60000, // Interval between Bluetooth scans
-    scanTimeout: process.env.BLUETOOTH_SCAN_TIMEOUT || 30000, // the timeout of each bluetooth scan
-    gladysUrl: process.env.GLADYS_URL || 'http://localhost:8080', // the URL of your main Gladys Instance
-    token: process.env.GLADYS_TOKEN || 'your-gladys-token', // your gladys security token. You can find it in Gladys Dashboard "Parameters" => "Security". 
-    gladysHouseId: process.env.GLADYS_HOUSE_ID || 1, // the ID of the house where this Bluetooth Scanner is located
+  scanInterval: process.env.BLUETOOTH_SCAN_INTERVAL || 60000, // Interval between Bluetooth scans
+  scanTimeout: process.env.BLUETOOTH_SCAN_TIMEOUT || 30000, // the timeout of each bluetooth scan
+  gladysUrl: process.env.GLADYS_URL || 'http://localhost:8080', // the URL of your main Gladys Instance
+  token: process.env.GLADYS_TOKEN || 'your-gladys-token', // your gladys security token. You can find it in Gladys Dashboard "Parameters" => "Security". 
+  gladysHouseId: process.env.GLADYS_HOUSE_ID || 1, // the ID of the house where this Bluetooth Scanner is located
+  services: 'bluetooth,miflora', // the name of devices services (bluetooth by default).
 };
