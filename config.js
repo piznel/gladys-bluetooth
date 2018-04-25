13ac96798978a0edf2b80fc9eb666f87120fae4d
Dubois Nicolas <dubois.nicolas@free.fr>
2018-04-25 23:38:53 +0200

    add flowerpower to config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   gladysUrl: process.env.GLADYS_URL || 'http://localhost:8080', // the URL of your main Gladys Instance
   token: process.env.GLADYS_TOKEN || 'YOUR-GLADYS-TOKEN', // your gladys security token. You can find it in Gladys Dashboard "Parameters" => "Security". 
   gladysHouseId: process.env.GLADYS_HOUSE_ID || 1, // the ID of the house where this Bluetooth Scanner is located
-  services: 'bluetooth,miflora,xiaomiht', // the name of devices services (bluetooth by default).
+  services: 'bluetooth,miflora,xiaomiht,flowerpower', // the name of devices services (bluetooth by default).
 };
